923b491a5a04ad10c4dbbf9cc16005eb03788bee
Yong Choon Kit <raphaelxkit@gmail.com>
2024-01-15 01:29:07 +0800

    Transaction Object Firebase

---
+++ app/src/main/java/com/example/trainticketsystem_hashmapbeatstherest/object/Transaction.java
@@ -0,0 +1,50 @@
+package com.example.trainticketsystem_hashmapbeatstherest.object;
+
+public class Transaction {
+    private String transactionID;
+    private String transactionType;
+    private String transactionAmount;
+    private String transactionTime;
+
+    public Transaction() {
+    }
+
+    public Transaction(String transactionID, String transactionType, String transactionAmount, String transactionTime) {
+        this.transactionID = transactionID;
+        this.transactionType = transactionType;
+        this.transactionAmount = transactionAmount;
+        this.transactionTime = transactionTime;
+    }
+
+    public String getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(String transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public String getTransactionType() {
+        return transactionType;
+    }
+
+    public void setTransactionType(String transactionType) {
+        this.transactionType = transactionType;
+    }
+
+    public String getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(String transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+    public String getTransactionTime() {
+        return transactionTime;
+    }
+
+    public void setTransactionTime(String transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+}
